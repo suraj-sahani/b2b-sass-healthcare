7ffafb0b99f3b01b1dd94654084673193134b25a
suraj-sahani <suraj.sahani.961@gmail.com>
2026-03-22 09:24:39 +0530

    chore: Add sign-in with google method

--- hooks/use-session.ts
+++ hooks/use-session.ts
@@ -2,22 +2,26 @@
 import { auth } from "@/lib/firebase/client";
 import { deleteSession, getSession } from "@/lib/firebase/session";
 import { useAuthStore } from "@/lib/store/use-auth-store";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { useEffect } from "react";
+import { toast } from "sonner";
 
 export function useSession() {
   const { user, isLoading, isAuthenticated, setUser, setLoading, clearAuth } =
     useAuthStore();
 
   useEffect(() => {
-    // Sync Firebase auth state → Zustand + session cookie
     const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
       if (firebaseUser) {
         const idToken = await firebaseUser.getIdToken();
 
         // Need to verify is the session is still valid.
         const session = await getSession();
-        // If it isn't, logout the user
         if (!session.success) {
           await deleteSession();
         }
@@ -37,9 +41,35 @@ export function useSession() {
     return () => unsubscribe();
   }, [setUser, clearAuth]);
 
+  const signInWithGoogle = async () => {
+    try {
+      setLoading(true);
+      const provider = new GoogleAuthProvider();
+      const { user } = await signInWithPopup(auth, provider);
+      // Create a session cookie
+      const idToken = await user.getIdToken();
+
+      if (!idToken) return;
+
+      await fetch("/api/auth/session/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ idToken }),
+      });
+
+      toast.success("Login successful!");
+    } catch (error) {
+      console.error(error);
+      toast.error(error instanceof Error ? error.message : "Login failed!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     user,
     isLoading,
     isAuthenticated,
+    signInWithGoogle,
   };
 }
